34056db543f7471d70d65dd393099dc5deb5a5af
anky209e <nasaankit01@gmail.com>
2024-01-15 02:44:54 +0530

    added grading and fixed testcase

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -3,7 +3,8 @@ from core import db
 from core.apis import decorators
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
-from.schema import AssignmentSchema,AssignmentGradeSchema
+from core.models.teachers import Teacher
+from.schema import AssignmentSchema,AssignmentGradeSchema,TeacherSchema
 
 
 principal_assignment_resources = Blueprint('principal_assignment_resources',__name__)
@@ -15,3 +16,27 @@ def list_assignments(p):
     submitted_graded_assingments = Assignment.get_submitted_and_graded()
     submitted_graded_assingments_dump = AssignmentSchema().dump(submitted_graded_assingments, many=True)
     return APIResponse.respond(data=submitted_graded_assingments_dump)
+
+@principal_assignment_resources.route('/teachers', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def get_all_teachers(p):
+    """Returns list All teachers"""
+    all_teachers = Teacher.get_all()
+    all_teachers_dump = TeacherSchema().dump(all_teachers, many=True)
+    return APIResponse.respond(data=all_teachers_dump)
+
+@principal_assignment_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_assignment(p, incoming_payload):
+    """Grade an assignment"""
+    grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+
+    graded_assignment = Assignment.mark_grade(
+        _id=grade_assignment_payload.id,
+        grade=grade_assignment_payload.grade,
+        auth_principal=p
+    )
+    db.session.commit()
+    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
+    return APIResponse.respond(data=graded_assignment_dump)
